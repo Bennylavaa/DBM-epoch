562f9e9dca0cfd002e66ca824b1eb8276acc2a88
wtfblub <wtfblub@gmail.com>
2026-01-07 18:07:26 +0100

    Version bump

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,9 +82,9 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20250907234930"),
-	DisplayVersion = "10.1.13 alpha", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2024, 07, 20) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	Revision = parseCurseDate("20260107170028"),
+	DisplayVersion = "10.1.13 epoch-1", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2025, 01, 07) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
 local fakeBWVersion = 7558
